4af92b76c3f2f065b974de0196fbe2ec85761857
ch-tseng <ch-tseng@sunplusit.com>
2018-07-29 09:43:11 +0800

    update

--- pythonYolo/video_misoffice.py
+++ pythonYolo/video_misoffice.py
@@ -3,16 +3,28 @@ import imutils
 from pydarknet import Detector, Image
 import cv2
 
+FILE_OUTPUT = 'misoffice.avi'
+
 if __name__ == "__main__":
 
-    net = Detector(bytes("/home/chtseng/works/opencv-cascade/yolo-v2/cfg/tiny-yolo.cfg", encoding="utf-8"), bytes("/home/chtseng/works/opencv-cascade/yolo-v2/cfg/tiny-yolo_9900.weights", encoding="utf-8"), 0,
-                   bytes("/home/chtseng/works/opencv-cascade/yolo-v2/cfg/obj.data", encoding="utf-8"))
+    net = Detector(bytes("cfg.misoffice/tiny-yolo.cfg", encoding="utf-8"), bytes("cfg.misoffice/tiny-yolo_18200.weights", encoding="utf-8"), 0,
+                   bytes("cfg.misoffice/obj.data", encoding="utf-8"))
 
-    cap = cv2.VideoCapture("/media/sf_VMshare/Videos/misoffice_3x.mp4")
+    cap = cv2.VideoCapture("cfg.misoffice/mis-office-demo.mov")
+
+    # Get current width of frame
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+    # Get current height of frame
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
+
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(FILE_OUTPUT,fourcc, 20.0, (int(width),int(height)))
+    i = 0
 
     while True:
         r, frame = cap.read()
         if r:
+            i += 1
             start_time = time.time()
 
             # Only measure the time taken by YOLO and API Call overhead
@@ -22,7 +34,7 @@ if __name__ == "__main__":
             del dark_frame
 
             end_time = time.time()
-            print("Elapsed Time:",end_time-start_time)
+            print("{}: Elapsed Time:{}".format(i, end_time-start_time) )
 
             for cat, score, bounds in results:
                 label = cat.decode('utf-8')
@@ -33,9 +45,13 @@ if __name__ == "__main__":
                     cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
                     cv2.putText(frame, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
 
-            frame = imutils.resize(frame, width=640)
-            cv2.imshow("preview", frame)
+            # Saves for video
+            out.write(frame)
+            #frame = imutils.resize(frame, width=640)
+            #cv2.imshow("preview", frame)
 
-        k = cv2.waitKey(1)
-        if k == 0xFF & ord("q"):
-            break
+
+        #k = cv2.waitKey(1)
+        #if k == 0xFF & ord("q"):
+        #    out.release()
+        #    break
--- pythonYolo/video_palm.py
+++ pythonYolo/video_palm.py
@@ -3,14 +3,14 @@ import imutils
 from pydarknet import Detector, Image
 import cv2
 
-FILE_OUTPUT = 'output.avi'
+FILE_OUTPUT = 'palm.avi'
 
 if __name__ == "__main__":
 
-    net = Detector(bytes("/home/chtseng/works/opencv-cascade-make/yolo-v2/cfg.palm/yolov3.cfg", encoding="utf-8"), bytes("/home/chtseng/works/opencv-cascade-make/yolo-v2/cfg.palm/yolov3_9900.weights", encoding="utf-8"), 0,
-                   bytes("/home/chtseng/works/opencv-cascade-make/yolo-v2/cfg.palm/obj.data", encoding="utf-8"))
+    net = Detector(bytes("cfg.palm/yolov3.cfg", encoding="utf-8"), bytes("cfg.palm/yolov3_9900.weights", encoding="utf-8"), 0,
+                   bytes("cfg.palm/obj.data", encoding="utf-8"))
 
-    cap = cv2.VideoCapture("/media/sf_VMshare/palm_test/IMG_3556.m4v")
+    cap = cv2.VideoCapture("cfg.palm/IMG_3558.m4v")
     # Get current width of frame
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
     # Get current height of frame
@@ -18,11 +18,12 @@ if __name__ == "__main__":
 
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(FILE_OUTPUT,fourcc, 20.0, (int(width),int(height)))
-
+    i = 0
 
     while True:
         r, frame = cap.read()
         if r:
+            i += 1
             start_time = time.time()
 
             # Only measure the time taken by YOLO and API Call overhead
@@ -32,7 +33,7 @@ if __name__ == "__main__":
             del dark_frame
 
             end_time = time.time()
-            print("Elapsed Time:",end_time-start_time)
+            print("{}: Elapsed Time:{}".format(i, end_time-start_time) )
 
             for cat, score, bounds in results:
                 label = cat.decode('utf-8')
@@ -41,15 +42,15 @@ if __name__ == "__main__":
                 if(label!="head"):
                     x, y, w, h = bounds
                     cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
-                    cv2.putText(frame, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+                    cv2.putText(frame, label, (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
 
             # Saves for video
             out.write(frame)
-            frame = imutils.resize(frame, width=640)
-            cv2.imshow("preview", frame)
+            #frame = imutils.resize(frame, width=640)
+            #cv2.imshow("preview", frame)
 
 
-        k = cv2.waitKey(1)
-        if k == 0xFF & ord("q"):
-            out.release()
-            break
+        #k = cv2.waitKey(1)
+        #if k == 0xFF & ord("q"):
+        #    out.release()
+        #    break
