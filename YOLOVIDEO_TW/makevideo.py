137a2bb3955bf58613ae5f76c0b976b4f961f22c
ch-tseng <ch.tseng@sunplusit.com>
2018-09-17 00:44:44 +0800

    Update makevideo.py

--- YOLOVIDEO_TW/makevideo.py
+++ YOLOVIDEO_TW/makevideo.py
@@ -2,8 +2,8 @@ from pydarknet import Detector, Image
 import imutils
 import cv2
 
-video_file = "/media/sf_VMshare/IMG_7155.MOV"
-output_video = "loofah.avi"
+video_file = "/media/sf_ShareFolder/videos/p_5.m4v"
+output_video = "/media/sf_ShareFolder/pepper_1.avi"
 
 def yoloPython(img):
     img2 = Image(img)
@@ -11,38 +11,53 @@ def yoloPython(img):
 
     for cat, score, bounds in results:
         cat = cat.decode("utf-8")
-        if(cat == "flower"):
-            boundcolor = (252, 172, 1)
-        elif(cat == "loofah"):
-            boundcolor = (1, 237, 252)
-        elif(cat == "loofah_wrapped"):
-            boundcolor = (1, 237, 252)
-        elif(cat == "loofah_under"):
-            boundcolor = (1, 237, 252)
+        print(cat)
+        if(cat == "0_pepper_flower"):
+            boundcolor = (255, 255, 255)
+            labelName = "flower"
+        elif(cat == "1_pepper_young"):
+            boundcolor = (193, 161, 31)
+            labelName = "young"
+        elif(cat == "2_pepper_matured"):
+            boundcolor = (12, 255, 240)
+            labelName = "pepper"
+        else:
+            boundcolor = (255, 255, 255)
+            labelName = "unknow"
 
         x, y, w, h = bounds
         cv2.rectangle(img, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), 
             int(y + h / 2)), boundcolor, thickness=3)
 
-        boundbox = cv2.imread("images/"+cat+".png")
-        print("read:","images/"+cat+".png")
+        boundbox = cv2.imread("cfg.pepper/images/"+cat+".jpg")
+        print("read:","images/"+cat+".jpg")
         print("boundbox:", boundbox.shape)
         start_x=int(x - w / 2)
         start_y=int(y - h / 2)-boundbox.shape[0]
+        '''
+        if(start_x<0): start_x = 0
+        if(start_y<0): start_y = 0
+        end_x=start_x+boundbox.shape[1]
+        end_y=start_y+boundbox.shape[0]
+        if(end_x>img.shape[1]): end_x = img.shape[1]
+        if(end_y>img.shape[0]): end_y = img.shape[0]
+        '''
         end_x=start_x+boundbox.shape[1]
         end_y=start_y+boundbox.shape[0]
 
-        if(start_y<0): start_y=0
-        if(start_x<0): start_x=0
         print("start_x={}, start_y={}, end_x={}, end_y={}".format(start_x,start_y,end_x,end_y))
-        #boundbox = imutils.resize(boundbox, width=newshape[1], height=newshape[0])
-        img[ start_y:end_y, start_x:end_x] = boundbox
+
+        if((end_x-start_x)!=img.shape[1]) or ((end_y-start_y)!=img.shape[0]):
+            cv2.putText(img, labelName, (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1.6, boundcolor, 2)
+
+        else:
+            img[ start_y:end_y, start_x:end_x] = boundbox
 
     return img
 
-net = Detector(bytes("cfg.loofah/yolov3-tiny.cfg", encoding="utf-8"),
-    bytes("cfg.loofah/yolov3-tiny_10000.weights", encoding="utf-8"), 0,
-    bytes("cfg.loofah/obj.data",encoding="utf-8"))
+net = Detector(bytes("cfg.pepper/yolov3.cfg", encoding="utf-8"),
+    bytes("cfg.pepper/weights/yolov3_10000.weights", encoding="utf-8"), 0,
+    bytes("cfg.pepper/obj.data",encoding="utf-8"))
 
 cap = cv2.VideoCapture(video_file)
 # Get current width of frame
