3a3bc91d58e53f57bcca19f4d864aaf137a23db5
digits <ch.tseng@sunplusit.com>
2018-09-21 18:16:31 +0800

    update

--- opencvTracking/multi-tracking.py
+++ opencvTracking/multi-tracking.py
@@ -1,139 +1,154 @@
-# import the necessary packages
-from imutils.video import VideoStream
-from imutils.video import FPS
-import argparse
+import time, sys
 import imutils
-import time
+from pydarknet import Detector, Image
 import cv2
+from random import randint
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-	help="path to input video file")
-ap.add_argument("-t", "--tracker", type=str, default="kcf",
-	help="OpenCV object tracker type")
-args = vars(ap.parse_args())
+FILE_OUTPUT = 'cucumber.avi'
+frames_tracking = 60
+trackerType = "CSRT"
 
-# extract the OpenCV version info
-(major, minor) = cv2.__version__.split(".")[:2]
- 
-# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
-# function to create our object tracker
-if int(major) == 3 and int(minor) < 3:
-	tracker = cv2.Tracker_create(args["tracker"].upper())
- 
-# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
-# approrpiate object tracker constructor:
-else:
-	# initialize a dictionary that maps strings to their corresponding
-	# OpenCV object tracker implementations
-	OPENCV_OBJECT_TRACKERS = {
-		"csrt": cv2.TrackerCSRT_create,
-		"kcf": cv2.TrackerKCF_create,
-		"boosting": cv2.TrackerBoosting_create,
-		"mil": cv2.TrackerMIL_create,
-		"tld": cv2.TrackerTLD_create,
-		"medianflow": cv2.TrackerMedianFlow_create,
-		"mosse": cv2.TrackerMOSSE_create,
-                "goturn": cv2.TrackerGOTURN_create
-	}
- 
-	# grab the appropriate object tracker using our dictionary of
-	# OpenCV object tracker objects
-	#tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
-	trackers = cv2.MultiTracker_create()
+multiTracker = cv2.MultiTracker_create()
+trackerTypes = ['BOOSTING', 'MIL', 'KCF','TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
 
- 
-# initialize the bounding box coordinates of the object we are going
-# to track
-initBB = None
+def createTrackerByName(trackerType):
+    # Create a tracker based on tracker name
+    if trackerType == trackerTypes[0]:
+        tracker = cv2.TrackerBoosting_create()
+    elif trackerType == trackerTypes[1]: 
+        tracker = cv2.TrackerMIL_create()
+    elif trackerType == trackerTypes[2]:
+        tracker = cv2.TrackerKCF_create()
+    elif trackerType == trackerTypes[3]:
+        tracker = cv2.TrackerTLD_create()
+    elif trackerType == trackerTypes[4]:
+        tracker = cv2.TrackerMedianFlow_create()
+    elif trackerType == trackerTypes[5]:
+        tracker = cv2.TrackerGOTURN_create()
+    elif trackerType == trackerTypes[6]:
+        tracker = cv2.TrackerMOSSE_create()
+    elif trackerType == trackerTypes[7]:
+        tracker = cv2.TrackerCSRT_create()
+    else:
+        tracker = None
+        print('Incorrect tracker name')
+        print('Available trackers are:')
+        for t in trackerTypes:
+            print(t)
+     
+    return tracker
 
-# if a video path was not supplied, grab the reference to the web cam
-if not args.get("video", False):
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(1.0)
- 
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
- 
-# initialize the FPS throughput estimator
-fps = None
+if __name__ == "__main__":
 
-# loop over frames from the video stream
-while True:
-	# grab the current frame, then handle if we are using a
-	# VideoStream or VideoCapture object
-	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
- 
-	# check to see if we have reached the end of the stream
-	if frame is None:
-		break
- 
-	# resize the frame (so we can process it faster) and grab the
-	# frame dimensions
-	frame = imutils.resize(frame, width=700)
-	(H, W) = frame.shape[:2]
+    net = Detector(bytes("../../darknet/cfg/yolov3.cfg", encoding="utf-8"), 
+                   bytes("../../darknet/yolov3.weights", encoding="utf-8"), 0,
+                   bytes("coco.data", encoding="utf-8"))
 
-	# check to see if we are currently tracking an object
-	if initBB is not None:
-		# grab the new bounding box coordinates of the object
-		(success, box) = tracker.update(frame)
- 
-		# check to see if the tracking was a success
-		if success:
-			(x, y, w, h) = [int(v) for v in box]
-			cv2.rectangle(frame, (x, y), (x + w, y + h),
-				(0, 255, 0), 2)
- 
-		# update the FPS counter
-		fps.update()
-		fps.stop()
- 
-		# initialize the set of information we'll be displaying on
-		# the frame
-		info = [
-			("Tracker", args["tracker"]),
-			("Success", "Yes" if success else "No"),
-			("FPS", "{:.2f}".format(fps.fps())),
-		]
- 
-		# loop over the info tuples and draw them on our frame
-		for (i, (k, v)) in enumerate(info):
-			text = "{}: {}".format(k, v)
-			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    cap = cv2.VideoCapture("cars.mp4")
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the 's' key is selected, we are going to "select" a bounding
-	# box to track
-	if key == ord("s"):
-		# select the bounding box of the object we want to track (make
-		# sure you press ENTER or SPACE after selecting the ROI)
-		initBB = cv2.selectROI("Frame", frame, fromCenter=False,
-			showCrosshair=True)
- 
-		# start OpenCV object tracker using the supplied bounding box
-		# coordinates, then start the FPS throughput estimator as well
-		tracker.init(frame, initBB)
-		fps = FPS().start()
+    # Get current width of frame
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+    # Get current height of frame
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
 
-	# if the `q` key was pressed, break from the loop
-	elif key == ord("q"):
-		break
- 
-# if we are using a webcam, release the pointer
-if not args.get("video", False):
-	vs.stop()
- 
-# otherwise, release the file pointer
-else:
-	vs.release()
- 
-# close all windows
-cv2.destroyAllWindows()
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(FILE_OUTPUT,fourcc, 20.0, (int(width),int(height)))
+    trackingIndex = 30
+    i = 0
+    bboxes = []
+    colors = []
+    counted = []
+    objid = []
+    lastY = []
+    lastX = []
+
+    while True:
+        r, frame = cap.read()
+        if r:
+            i += 1
+            trackingIndex += 1
+            start_time = time.time()
+
+            # Only measure the time taken by YOLO and API Call overhead
+
+            if(trackingIndex>=frames_tracking):
+                trackingIndex = 31
+                del multiTracker
+                bboxes = []
+                colors = []
+                multiTracker = cv2.MultiTracker_create()
+
+                dark_frame = Image(frame)
+                results = net.detect(dark_frame)
+                del dark_frame
+
+                end_time = time.time()
+                print("{}: Elapsed Time:{}".format(i, end_time-start_time) )
+
+                indexObj = 0
+                for cat, score, bounds in results:
+                    label = cat.decode('utf-8')
+                    print("{}:{}".format(cat, score))
+
+                    x, y, w, h = bounds
+                    if(label=="car"):
+                        id = str(time.time()) + "_" + str(indexObj)
+                        bboxes.append((x-(w/2), y-(h/2), w, h))
+                        colors.append((randint(0, 255), randint(0, 255), randint(0, 255)))
+                        objid.append(id)
+                        counted.append(0)
+                        lastY.append(y)
+                        lastX.append(x)
+
+                        cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(0,255,0), 3)
+                        print("add text: ",label)
+                        cv2.putText(frame, label, (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0,255,0))
+                        indexObj += 1
+
+                for bbox in bboxes:
+                    multiTracker.add(createTrackerByName(trackerType), frame, bbox)
+
+        success, boxes = multiTracker.update(frame)
+        for id, newbox in enumerate(boxes):
+            p1 = (int(newbox[0]), int(newbox[1]))
+            p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
+            #cv2.rectangle(frame, p1, p2, colors[id], 2, 1)
+            #cv2.putText(frame, objid[id], (int(newbox[0]), int(newbox[1])), cv2.FONT_HERSHEY_COMPLEX, 1.0, colors[id])
+
+            if( newbox[1] > 10): # Y
+                counted[id] = 1
+
+                direction = ""
+                if( int(newbox[0]+(newbox[2]/2)) - lastX[id] > 2 ):
+                    direction = direction + "Right"
+                elif( int(newbox[0]+(newbox[2]/2)) - lastX[id] < -2):
+                    direction = direction + "Left"
+                else:
+                    direction = direction + "Stoped"
+
+                if( int(newbox[1]+(newbox[3]/2)) - lastY[id] > 2 ):
+                    direction = direction + "/Down"
+                elif( int(newbox[1]+(newbox[3]/2)) - lastY[id] < -2):
+                    direction = direction + "/Up"
+                else:
+                    direction = direction + "Stoped"
+
+                if("Stop" not in direction):
+                    cv2.putText(frame, direction, (int(newbox[0]), int(newbox[1]+20)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), 2)
+                    lastX[id] = int(newbox[0]+(newbox[2]/2))
+                    lastY[id] = int(newbox[1]+(newbox[3]/2))
+
+        #frame = imutils.resize(frame, width=640)
+        print("Frame #{}, trackingIndex:{}, bbox:{}, obj count:{}".format(i, trackingIndex, len(bboxes), len(boxes)) )
+        cv2.imshow("preview", frame)
+
+        # Saves for video
+        out.write(frame)
+        if(trackingIndex==31):
+            k = cv2.waitKey(1)
+        else:
+            k = cv2.waitKey(1)
+
+        if k == 0xFF & ord("q"):
+            out.release()
+            break
